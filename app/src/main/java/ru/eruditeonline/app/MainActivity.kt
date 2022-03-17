c6126a06fc271250d5c40a5e03aa3f7fec6f0bac
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-03-17 23:19:47 +0400

    init project

---
+++ app/src/main/java/ru/eruditeonline/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.eruditeonline.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
