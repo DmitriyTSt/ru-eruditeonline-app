cb858e2ea66902a53a25269fde16b9baa4f0558a
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-04-03 22:09:57 +0400

    fix hardcode token

--- app/src/main/java/ru/eruditeonline/app/data/repository/TokenRepositoryImpl.kt
+++ app/src/main/java/ru/eruditeonline/app/data/repository/TokenRepositoryImpl.kt
@@ -6,8 +6,6 @@ import javax.inject.Inject
 class TokenRepositoryImpl @Inject constructor(
     preferencesStorage: PreferencesStorage,
 ) : TokenRepository {
-    override var accessToken: String? =
-        "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiZXhwIjoxNjQ5MDMwODcxfQ.N3pmHjHhJ6rFmOmWgNnBj-" +
-            "pp0zJIY89CtrEGMmkxB6GLkYIGmWBVOf13i4FNt83cIgjmeIWA1T4LANHaxxxYLw" // preferencesStorage.accessToken
+    override var accessToken = preferencesStorage.accessToken
     override var refreshToken = preferencesStorage.refreshToken
 }
