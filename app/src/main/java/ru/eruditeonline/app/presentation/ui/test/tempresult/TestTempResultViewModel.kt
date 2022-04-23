77213e2a14464d74b21c3a7c9ffb6ff97dfac228
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-04-23 12:27:10 +0400

    add temp result layout

--- app/src/main/java/ru/eruditeonline/app/presentation/ui/test/tempresult/TestTempResultViewModel.kt
+++ app/src/main/java/ru/eruditeonline/app/presentation/ui/test/tempresult/TestTempResultViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import ru.eruditeonline.app.data.model.LoadableState
 import ru.eruditeonline.app.data.model.competition.CompetitionPassData
 import ru.eruditeonline.app.data.model.test.TempResult
+import ru.eruditeonline.app.domain.usecase.result.SaveResultUseCase
 import ru.eruditeonline.app.domain.usecase.test.CheckTestUseCase
 import ru.eruditeonline.app.presentation.ui.base.BaseViewModel
 import javax.inject.Inject
 
 class TestTempResultViewModel @Inject constructor(
     private val checkTestUseCase: CheckTestUseCase,
+    private val saveResultUseCase: SaveResultUseCase,
 ) : BaseViewModel() {
 
     /** Проверка прохождения теста */
