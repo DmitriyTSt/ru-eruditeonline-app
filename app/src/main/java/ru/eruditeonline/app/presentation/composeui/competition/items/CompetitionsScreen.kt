aec09940d351968d2624bf3e841a8b31b01d5d49
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2023-11-05 01:12:23 +0400

    add competition items view type

--- app/src/main/java/ru/eruditeonline/app/presentation/composeui/competition/items/CompetitionsScreen.kt
+++ app/src/main/java/ru/eruditeonline/app/presentation/composeui/competition/items/CompetitionsScreen.kt
@@ -4,18 +4,27 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.asFlow
@@ -25,9 +34,11 @@ import ru.eruditeonline.app.R
 import ru.eruditeonline.app.presentation.composeui.model.Screen
 import ru.eruditeonline.app.presentation.composeui.paging.PagingStateFlipperView
 import ru.eruditeonline.app.presentation.composeui.paging.applyFooterState
+import ru.eruditeonline.app.presentation.composeui.theme.AppTypography
 import ru.eruditeonline.app.presentation.composeui.views.CompetitionItemBigGridView
-import ru.eruditeonline.app.presentation.composeui.views.TopAppBarView
+import ru.eruditeonline.app.presentation.composeui.views.CompetitionItemSmallRowView
 import ru.eruditeonline.app.presentation.ui.competition.items.CompetitionItemsViewModel
+import ru.eruditeonline.app.presentation.ui.competition.items.CompetitionItemsViewType
 import kotlin.random.Random
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,6 +47,7 @@ fun CompetitionsScreen(navController: NavController, viewModel: CompetitionItems
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
     val competitionPagingItems = viewModel.pagingDataLiveData.asFlow().collectAsLazyPagingItems()
+    val listViewType by viewModel.listViewTypeLiveData.observeAsState(CompetitionItemsViewType.CARD)
 
     LaunchedEffect(Unit) {
         viewModel.callOperations {
@@ -45,9 +57,32 @@ fun CompetitionsScreen(navController: NavController, viewModel: CompetitionItems
 
     Scaffold(
         topBar = {
-            TopAppBarView(
-                title = stringResource(id = R.string.competition_items_title),
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.competition_items_title),
+                        style = AppTypography.titleLarge,
+                    )
+                },
                 scrollBehavior = scrollBehavior,
+                actions = {
+                    IconButton(onClick = { viewModel.changeListViewType() }) {
+                        val viewTypeIconRes = when (listViewType) {
+                            CompetitionItemsViewType.CARD -> R.drawable.ic_view_type_card
+                            CompetitionItemsViewType.ROW -> R.drawable.ic_view_type_row
+                        }
+                        Icon(
+                            painter = painterResource(id = viewTypeIconRes),
+                            contentDescription = null,
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_filter),
+                            contentDescription = null,
+                        )
+                    }
+                }
             )
         },
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -68,18 +103,42 @@ fun CompetitionsScreen(navController: NavController, viewModel: CompetitionItems
                 columns = GridCells.Fixed(spanCount),
                 contentPadding = PaddingValues(12.dp),
             ) {
-                items(competitionPagingItems.itemCount) { index ->
-                    val competitionItem = competitionPagingItems[index]
-                    if (competitionItem != null) {
-                        CompetitionItemBigGridView(
-                            competitionItem = competitionItem,
-                            modifier = Modifier
-                                .padding(4.dp)
-                                .fillMaxHeight(),
-                            onClick = {
-                                navController.navigate(Screen.Competition.route(Random.nextInt()))
+
+                when (listViewType) {
+                    CompetitionItemsViewType.CARD -> {
+                        items(competitionPagingItems.itemCount) { index ->
+                            val competitionItem = competitionPagingItems[index]
+                            if (competitionItem != null) {
+                                CompetitionItemBigGridView(
+                                    competitionItem = competitionItem,
+                                    modifier = Modifier
+                                        .padding(4.dp)
+                                        .fillMaxHeight(),
+                                    onClick = {
+                                        navController.navigate(Screen.Competition.route(Random.nextInt()))
+                                    }
+                                )
                             }
-                        )
+                        }
+                    }
+                    CompetitionItemsViewType.ROW -> {
+                        items(
+                            count = competitionPagingItems.itemCount,
+                            span = { GridItemSpan(2) },
+                        ) { index ->
+                            val competitionItem = competitionPagingItems[index]
+                            if (competitionItem != null) {
+                                CompetitionItemSmallRowView(
+                                    competitionItem = competitionItem,
+                                    modifier = Modifier
+                                        .padding(4.dp)
+                                        .fillMaxWidth(),
+                                    onClick = {
+                                        navController.navigate(Screen.Competition.route(Random.nextInt()))
+                                    }
+                                )
+                            }
+                        }
                     }
                 }
 
---
+++ app/src/main/java/ru/eruditeonline/app/presentation/composeui/views/CompetitionItemSmallRowView.kt
@@ -0,0 +1,67 @@
+package ru.eruditeonline.app.presentation.composeui.views
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.eruditeonline.app.data.model.competition.CompetitionItemShort
+import ru.eruditeonline.app.presentation.composeui.theme.AppTypography
+
+@Composable
+fun CompetitionItemSmallRowView(
+    competitionItem: CompetitionItemShort,
+    onClick: (CompetitionItemShort) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Card(modifier = modifier) {
+        Row(modifier = Modifier.clickable { onClick(competitionItem) }) {
+            CompetitionImage(
+                imageUrl = competitionItem.icon.orEmpty(),
+                borderStroke = 6.dp,
+                backgroundColor = MaterialTheme.colorScheme.surfaceVariant,
+                modifier = Modifier
+                    .padding(start = 8.dp, top = 8.dp)
+                    .size(64.dp),
+            )
+            Column(
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 12.dp),
+            ) {
+                CompetitionDifficultyView(
+                    difficulty = competitionItem.difficulty,
+                )
+                Text(
+                    text = competitionItem.title,
+                    style = AppTypography.bodyMedium,
+                    modifier = Modifier.padding(top = 4.dp),
+                )
+            }
+        }
+        Text(
+            text = competitionItem.ages,
+            modifier = Modifier.padding(top = 4.dp, start = 12.dp, end = 12.dp, bottom = 8.dp),
+            style = AppTypography.bodySmall,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CompetitionItemSmallRowViewPreview() {
+    val item = CompetitionItemShort(
+        id = 0,
+        title = "«Золотая осень»",
+        subject = "Литература",
+        ages = "5-11 классы, студенты, учителя",
+        difficulty = 2,
+        icon = "https://erudit-online.ru/assets/images/1/gold_autumn-9c6de671.jpg",
+    )
+    CompetitionItemSmallRowView(item, {})
+}
