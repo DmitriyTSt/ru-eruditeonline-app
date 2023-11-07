cfde57527513f002e9a313103cefe8634c0dda0b
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2023-11-07 22:13:42 +0400

    add comment

--- app/src/main/java/ru/eruditeonline/app/presentation/composeui/base/ComposeHackArguments.kt
+++ app/src/main/java/ru/eruditeonline/app/presentation/composeui/base/ComposeHackArguments.kt
@@ -5,6 +5,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.NavController
 
+//
+// TODO Этот файл - костыли, пока не придумаю нормальную типизированную удобную навигацию по роутам
+//
+
 inline fun <reified T : Parcelable> NavController.setNextScreenArguments(arg: T) {
     currentBackStackEntry?.savedStateHandle?.set(T::class.java.name, arg)
 }
