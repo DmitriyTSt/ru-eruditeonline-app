65e363761cdbfe1dc04c7f15e111c2c0749fdc8e
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2023-11-11 17:08:07 +0400

    add test login screen

--- app/src/main/java/ru/eruditeonline/app/presentation/composeui/auth/login/LoginScreen.kt
+++ app/src/main/java/ru/eruditeonline/app/presentation/composeui/auth/login/LoginScreen.kt
@@ -1,9 +1,147 @@
 package ru.eruditeonline.app.presentation.composeui.auth.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import ru.eruditeonline.app.R
+import ru.eruditeonline.app.presentation.composeui.base.ObserveDestinations
+import ru.eruditeonline.app.presentation.composeui.theme.AppTypography
+import ru.eruditeonline.app.presentation.composeui.views.NavigationIcon
 import ru.eruditeonline.app.presentation.ui.auth.login.LoginViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(navController: NavController, viewModel: LoginViewModel) {
+    ObserveDestinations(navController, viewModel)
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    var login by remember { mutableStateOf("") }
+    var isLoginError by remember { mutableStateOf(false) }
+    var loginError by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isPasswordError by remember { mutableStateOf(false) }
+    var passwordError by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    NavigationIcon(navController)
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        contentWindowInsets = WindowInsets.ime,
+    ) { innerPaddings ->
+        Column(
+            modifier = Modifier
+                .padding(innerPaddings)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+        ) {
+            Image(
+                painter = painterResource(R.drawable.img_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(150.dp)
+                    .align(Alignment.CenterHorizontally),
+            )
+            Text(
+                text = stringResource(R.string.login_title),
+                style = AppTypography.titleLarge,
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .align(Alignment.CenterHorizontally),
+            )
+            TextField(
+                value = login,
+                onValueChange = {
+                    login = it
+                    isLoginError = it.length > 5
+                    loginError = "Ошибка логина".takeIf { isLoginError }.orEmpty()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 16.dp),
+                label = {
+                    Text(text = stringResource(R.string.login_email_hint))
+                },
+                isError = isLoginError,
+                supportingText = {
+                    Text(
+                        text = loginError,
+                        style = AppTypography.labelMedium,
+                        modifier = Modifier.fillMaxWidth(),
+                        color = MaterialTheme.colorScheme.error,
+                    )
+                },
+            )
+            TextField(
+                value = password,
+                onValueChange = {
+                    password = it
+                    isPasswordError = it.length > 5
+                    passwordError = "Ошибка пароля".takeIf { isPasswordError }.orEmpty()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 8.dp),
+                label = {
+                    Text(text = stringResource(R.string.login_password_hint))
+                },
+                isError = isPasswordError,
+                supportingText = {
+                    Text(
+                        text = passwordError,
+                        style = AppTypography.labelMedium,
+                        modifier = Modifier.fillMaxWidth(),
+                        color = MaterialTheme.colorScheme.error,
+                    )
+                },
+            )
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(text = stringResource(R.string.login_button_label), style = AppTypography.bodyMedium)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
 }
