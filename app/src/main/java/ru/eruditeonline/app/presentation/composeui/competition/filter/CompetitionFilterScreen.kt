99f6546d1cf19c938e65513242d0f027537e0f51
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2026-02-10 13:47:19 +0400

    new filters bottom design

--- app/src/main/java/ru/eruditeonline/app/presentation/composeui/competition/filter/CompetitionFilterScreen.kt
+++ app/src/main/java/ru/eruditeonline/app/presentation/composeui/competition/filter/CompetitionFilterScreen.kt
@@ -5,20 +5,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SecondaryScrollableTabRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
@@ -29,12 +31,15 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,8 +49,8 @@ import kotlinx.coroutines.flow.collectLatest
 import ru.eruditeonline.app.R
 import ru.eruditeonline.app.data.model.competition.CompetitionFilters
 import ru.eruditeonline.app.data.model.competition.FilterItem
-import ru.eruditeonline.app.presentation.composeui.base.LocalScreenResultDispatcher
 import ru.eruditeonline.app.presentation.composeui.base.LocalBackStack
+import ru.eruditeonline.app.presentation.composeui.base.LocalScreenResultDispatcher
 import ru.eruditeonline.app.presentation.composeui.base.ObserveDestinations
 import ru.eruditeonline.app.presentation.composeui.base.ScreenResultDispatcher
 import ru.eruditeonline.app.presentation.composeui.base.appViewModel
@@ -61,6 +66,11 @@ fun CompetitionFilterScreen(filters: CompetitionFilters, viewModel: CompetitionF
     viewModel.ObserveDestinations()
     val screenResultDispatcher = LocalScreenResultDispatcher.current
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    var applyButtonHeight by remember { mutableIntStateOf(0) }
+    val density = LocalDensity.current
+    val applyButtonHeightDp = remember(applyButtonHeight) {
+        with(density) { applyButtonHeight.toDp() }
+    }
 
     LaunchedEffect(filters) {
         viewModel.initFilters(filters)
@@ -95,43 +105,52 @@ fun CompetitionFilterScreen(filters: CompetitionFilters, viewModel: CompetitionF
             )
         },
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        contentWindowInsets = WindowInsets.statusBars,
+        contentWindowInsets = WindowInsets.systemBars,
     ) { innerPaddings ->
-        Column(Modifier.padding(innerPaddings)) {
-            FilterGroupTabs(
-                selectedTabIndex = selectedTabIndex,
-                onTabClick = {
-                    selectedTabIndex = it
-                },
-                filterGroups = filterGroups,
-                modifier = Modifier.zIndex(2f),
-            )
-
-            Box(
-                modifier = Modifier
-                    .padding(horizontal = 12.dp)
-                    .weight(1f),
+        Box(Modifier.fillMaxSize()) {
+            Column(
+                Modifier
+                    .padding(top = innerPaddings.calculateTopPadding())
             ) {
+                FilterGroupTabs(
+                    selectedTabIndex = selectedTabIndex,
+                    onTabClick = {
+                        selectedTabIndex = it
+                    },
+                    filterGroups = filterGroups,
+                    modifier = Modifier.zIndex(2f),
+                )
+
                 if (filterGroups.isNotEmpty()) {
                     val filters = filterGroups[selectedTabIndex].filters
                     FlowRow(
                         modifier = Modifier
+                            .padding(horizontal = 12.dp)
                             .verticalScroll(rememberScrollState()),
                         horizontalArrangement = Arrangement.Start,
                     ) {
                         filters.forEach { filterItem ->
                             FilterItemView(filterItem, viewModel::onFilterClick)
                         }
+                        Spacer(
+                            Modifier
+                                .fillMaxWidth()
+                                .height(innerPaddings.calculateBottomPadding() + applyButtonHeightDp + 16.dp)
+                        )
                     }
                 }
             }
 
-            Divider()
-            ElevatedButton(
+            Button(
                 onClick = { viewModel.applyFilter(filterGroups) },
                 modifier = Modifier
+                    .align(Alignment.BottomCenter)
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                    .padding(horizontal = 16.dp)
+                    .padding(bottom = innerPaddings.calculateBottomPadding() + 16.dp)
+                    .onSizeChanged {
+                        applyButtonHeight = it.height
+                    },
             ) {
                 Text(text = stringResource(R.string.filter_apply_button_label))
             }
@@ -146,15 +165,17 @@ private fun FilterGroupTabs(
     filterGroups: List<FilterGroup>,
     modifier: Modifier = Modifier,
 ) {
-    TabRow(selectedTabIndex = selectedTabIndex, modifier = modifier) {
-        filterGroups.forEachIndexed { index, filterGroup ->
-            Tab(
-                selected = index == selectedTabIndex,
-                onClick = { onTabClick(index) },
-                text = {
-                    Text(text = stringResource(filterGroup.titleRes))
-                }
-            )
+    if (filterGroups.isNotEmpty()) {
+        SecondaryScrollableTabRow(selectedTabIndex = selectedTabIndex, modifier = modifier) {
+            filterGroups.forEachIndexed { index, filterGroup ->
+                Tab(
+                    selected = index == selectedTabIndex,
+                    onClick = { onTabClick(index) },
+                    text = {
+                        Text(text = stringResource(filterGroup.titleRes))
+                    }
+                )
+            }
         }
     }
 }
